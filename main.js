87b4be690922219a912847fd6e6d828d835c71f2
Natalie <86774243+nnatalieh@users.noreply.github.com>
2023-05-27 21:04:28 -0500

    add new functions and update values

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const displayEquationDiv = document.querySelector(".display-equation");
+const displayTotalDiv = document.querySelector(".display-total");
+
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
@@ -6,94 +9,108 @@ const divide = (num1, num2) => num1 / num2;
 function calculator(num1, num2, operator) {
     if (operator === "+") return add(num1, num2);
     if (operator === "-") return subtract(num1, num2);
-    if (operator === "x") return multiply(num1, num2);
+    if (operator === "×") return multiply(num1, num2);
     if (operator === "÷") return divide(num1, num2);
 }
 
 // // KEYBOARD INPUT
-const displayInput = document.querySelector(".display");
-displayInput.addEventListener("keypress", function (e) {
+document.addEventListener("keypress", function (e) {
     let span = document.createElement("span");
     operatorList = ["+", "-", "x", "/"];
     if (operatorList.includes(e.key)) {
         span.classList.add("operator");
-    }
-    if (e.key === "/") {
-        displayInput.appendChild(span).innerText = "÷";
+        if (e.key === "/") {
+            displayEquationDiv.appendChild(span).innerText = " ÷ ";
+            displayEquationDiv.value += "÷";
+        } else if (e.key === "x") {
+            displayEquationDiv.appendChild(span).innerText = " × ";
+            displayEquationDiv.value += "×";
+        } else {
+            displayEquationDiv.appendChild(span).innerText = ` ${e.key} `;
+            displayEquationDiv.value += e.key;
+        }
     } else {
-        displayInput.appendChild(span).innerText = e.key;
+        displayEquationDiv.appendChild(span).innerText = e.key;
+        displayEquationDiv.value += e.key;
     }
 });
 
 // NUMBER BUTTONS
+const decimalButton = document.querySelector("#decimal");
+decimalButton.addEventListener("click", function (e) {
+    let span = document.createElement("span");
+    displayEquationDiv.appendChild(span).innerText = ".";
+    displayEquationDiv.value += ".";
+});
+
 const zeroButton = document.querySelector("#zero");
 zeroButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "0";
-    displayInput.value += "0";
+    displayEquationDiv.appendChild(span).innerText = "0";
+    displayEquationDiv.value += "0";
 });
 
 const oneButton = document.querySelector("#one");
 oneButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "1";
-    displayInput.value += "1";
+    displayEquationDiv.appendChild(span).innerText = "1";
+    displayEquationDiv.value += "1";
 });
 
 const twoButton = document.querySelector("#two");
 twoButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "2";
-    displayInput.value += "2";
+    displayEquationDiv.appendChild(span).innerText = "2";
+    displayEquationDiv.value += "2";
 });
 
 const threeButton = document.querySelector("#three");
 threeButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "3";
-    displayInput.value += "3";
+    displayEquationDiv.appendChild(span).innerText = "3";
+    displayEquationDiv.value += "3";
 });
 
 const fourButton = document.querySelector("#four");
 fourButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "4";
-    displayInput.value += "4";
+    displayEquationDiv.appendChild(span).innerText = "4";
+    displayEquationDiv.value += "4";
 });
 
 const fiveButton = document.querySelector("#five");
 fiveButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "5";
-    displayInput.value += "5";
+    displayEquationDiv.appendChild(span).innerText = "5";
+    displayEquationDiv.value += "5";
 });
 
 const sixButton = document.querySelector("#six");
 sixButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "6";
-    displayInput.value += "6";
+    displayEquationDiv.appendChild(span).innerText = "6";
+    displayEquationDiv.value += "6";
 });
 
 const sevenButton = document.querySelector("#seven");
 sevenButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "7";
-    displayInput.value += "7";
+    displayEquationDiv.appendChild(span).innerText = "7";
+    displayEquationDiv.value += "7";
 });
 
 const eightButton = document.querySelector("#eight");
 eightButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "8";
-    displayInput.value += "8";
+    displayEquationDiv.appendChild(span).innerText = "8";
+    displayEquationDiv.value += "8";
 });
 
 const nineButton = document.querySelector("#nine");
 nineButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
-    displayInput.appendChild(span).innerText = "9";
-    displayInput.value += "9";
+    displayEquationDiv.appendChild(span).innerText = "9";
+    displayEquationDiv.value += "9";
 });
 
 // OPERATION BUTTONS
@@ -101,38 +118,38 @@ const addButton = document.querySelector("#add");
 addButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
     span.classList.add("operator");
-    displayInput.appendChild(span).innerText = " + ";
-    displayInput.value += "+";
+    displayEquationDiv.appendChild(span).innerText = " + ";
+    displayEquationDiv.value += "+";
 });
 
 const subtractButton = document.querySelector("#subtract");
 subtractButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
     span.classList.add("operator");
-    displayInput.appendChild(span).innerText = " - ";
-    displayInput.value += "-";
+    displayEquationDiv.appendChild(span).innerText = " - ";
+    displayEquationDiv.value += "-";
 });
 
 const multiplyButton = document.querySelector("#multiply");
 multiplyButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
     span.classList.add("operator");
-    displayInput.appendChild(span).innerText = " x ";
-    displayInput.value += "x";
+    displayEquationDiv.appendChild(span).innerText = " × ";
+    displayEquationDiv.value += "×";
 });
 
 const divideButton = document.querySelector("#divide");
 divideButton.addEventListener("click", function (e) {
     let span = document.createElement("span");
     span.classList.add("operator");
-    displayInput.appendChild(span).innerText = " ÷ ";
-    displayInput.value += "÷";
+    displayEquationDiv.appendChild(span).innerText = " ÷ ";
+    displayEquationDiv.value += "÷";
 });
 
 // INITIALIZES CALCULATION
 const equalButton = document.querySelector("#equal");
 equalButton.addEventListener("click", function (e) {
-    let processedString = displayInput.textContent.split(" ");
+    let processedString = displayEquationDiv.textContent.split(" ");
     let total = 0;
     while (processedString.length > 1) {
         total = calculator(
@@ -144,16 +161,22 @@ equalButton.addEventListener("click", function (e) {
         processedString[0] = total;
     }
     // adds total to display view
-    displayInput.value = total;
+    displayTotalDiv.innerText = total;
 });
 
 // CLEARS CALCULATION
 const clearButton = document.querySelector("#clear");
-const clear = clearButton.addEventListener("click", function (e) {
+clearButton.addEventListener("click", function (e) {
     // removes display divs
-    while (displayInput.hasChildNodes()) {
-        displayInput.removeChild(displayInput.children[0]);
+    while (displayEquationDiv.hasChildNodes()) {
+        displayEquationDiv.removeChild(displayEquationDiv.children[0]);
     }
     // clears display view
-    displayInput.value = "";
+    displayTotalDiv.innerText = "";
+});
+
+// DELETES LAST ENTRY
+const deleteButton = document.querySelector("#delete");
+deleteButton.addEventListener("click", function (e) {
+    displayEquationDiv.removeChild(displayEquationDiv.lastChild);
 });
